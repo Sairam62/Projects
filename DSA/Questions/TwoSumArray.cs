7068e7cff8fc3fe58dc4c53cfd6c037b6d6c9e19
sairam <sairam04.ram@gmail.com>
2024-10-24 21:18:17 +0530

    Added logic for TwoSum

--- DSA/Questions/TwoSumArray.cs
+++ DSA/Questions/TwoSumArray.cs
@@ -1,9 +1,34 @@
+
 namespace DSA;
 
 public class TwoSumArray : BaseQuestion
 {
     public override void Handle()
     {
-        throw new NotImplementedException();
+        int[] arr = [2,7,11,15];
+        int req = 9;
+        int[] result = TwoSum(arr,req);
+    }
+
+    private int[] TwoSum(int[] arr, int req)
+    {
+        // Idea is to create a dictionary with req-arr[i]
+        // If any arr[i] present in dictionary then i and dictionary value is the required answer.
+        int rem = 0;
+        Dictionary<int,int> map = [];
+        for(int i=0;i<arr.Length;i++)
+        {
+            rem = req - arr[i];
+
+            if(map.TryGetValue(rem, out int value))
+            {
+                return [value, i];
+            }
+            else{
+                map[arr[i]]=i;
+            }
+        }
+
+        return [];
     }
 }
